ad086528eae8240a3b9256789faf874d1378cf78
ajmarrocco <ajmarrocco@gmail.com>
2022-04-08 22:16:06 -0400

    update single pizza

--- models/Comment.js
+++ models/Comment.js
@@ -1,33 +1,6 @@
 const { Schema, model, Types } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
-const CommentSchema = new Schema({
-    writtenBy: {
-        type: String
-    },
-    commentBody: {
-        type: String
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-        get: createdAtVal => dateFormat(createdAtVal)
-    },
-    replies: [ReplySchema]
-    },
-    {
-        toJSON: {
-            virtuals: true,
-            getters: true
-        },
-        id: false
-    }
-);
-
-CommentSchema.virtual('replyCount').get(function() {
-    return this.replies.length;
-});
-
 const ReplySchema = new Schema(
     {
         // set custom id to avoid confusion with parent comment _id
@@ -39,7 +12,7 @@ const ReplySchema = new Schema(
             type: String
         },
         writtenBy: {
-            type: String
+        type: String
         },
         createdAt: {
             type: Date,
@@ -53,6 +26,35 @@ const ReplySchema = new Schema(
         }
     }
 );
+
+const CommentSchema = new Schema(
+    {
+        writtenBy: {
+            type: String
+        },
+        commentBody: {
+            type: String
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        },
+        // use ReplySchema to validate data for a reply
+        replies: [ReplySchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        getters: true
+        },
+        id: false
+    }
+);
+
+CommentSchema.virtual('replyCount').get(function() {
+    return this.replies.length;
+});
 
 const Comment = model('Comment', CommentSchema);
 
